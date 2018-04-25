b6de175cb93415bb92da1cee83f69be7f25308d9
Wilhelm Ericsson <wilhelm__ericsson@hotmail.com>
2018-04-25 13:27:32 +0200

    QuestionView added

--- Client/src/pages/home/home.ts
+++ Client/src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Alert, AlertController} from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  
   constructor(public navCtrl: NavController) {
-
+    
+    
   }
+  
 
+  
 }
