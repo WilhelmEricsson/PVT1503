3212dd3c763d696a9f1a6dd613aaba5ea26ce8e9
Wilhelm Ericsson <wilhelm__ericsson@hotmail.com>
2018-04-26 12:37:24 +0200

    DOA and Question class added

---
+++ Server/src/main/java/com/pvt15/entity/Question.java
@@ -0,0 +1,18 @@
+package com.pvt15.entity;
+
+public class Question {
+	private int id;
+	private String category; // ska kanske vara en egen klass
+	private String question;
+	private String[] alternatives;
+	
+	//--------------------Constructor-------------------------
+	
+	
+	
+	
+	
+	//--------------------Methods-----------------------------
+		
+	
+}
