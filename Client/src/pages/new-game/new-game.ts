e08ac3f4ff09b4b27a3feec242d27ba4d67ded21
Andreas Engberg <AndreasEngberg96@outlook.com>
2018-04-26 14:53:38 +0200

    removed unused import

--- Client/src/pages/new-game/new-game.ts
+++ Client/src/pages/new-game/new-game.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AddFriendsPage } from '../add-friends/add-friends';
 import { PlayFriendsPage } from '../play-friends/play-friends';
 
 /**
