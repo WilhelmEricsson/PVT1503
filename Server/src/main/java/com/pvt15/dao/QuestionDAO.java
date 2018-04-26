bfc1226cbe313551bcb553af87e58d30537e0045
Wilhelm Ericsson <ericssonwilhelm@gmail.com>
2018-04-26 23:53:12 +0200

    Test mappningar i QuestionController

--- Server/src/main/java/com/pvt15/dao/QuestionDAO.java
+++ Server/src/main/java/com/pvt15/dao/QuestionDAO.java
@@ -1,17 +1,23 @@
 package com.pvt15.dao;
 
 
+import com.pvt15.entity.Question;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public class QuestionDAO {
 
-	
+	Question testQuestion;
 	
 	//--------------------Constructor-------------------------
 
     public QuestionDAO() {
+	    testQuestion = new Question(1,"testKate","Hur går det för oss=", new String[]{"okej!", "Dåligt", "Oklart", "Strålande"});
+    }
+
+    public Question getQuestionById(int i) {
+        return testQuestion;
     }
 
 
--- Server/src/main/java/com/pvt15/service/QuestionService.java
+++ Server/src/main/java/com/pvt15/service/QuestionService.java
@@ -1,14 +1,22 @@
 package com.pvt15.service;
 
+import com.pvt15.dao.QuestionDAO;
+import com.pvt15.entity.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionService {
-
+    @Autowired
+    private QuestionDAO questionDAO;
 
     //--------------------Constructor-------------------------
     public QuestionService() {
 
+    }
+
+    public Question getQuestionById(int i) {
+        return questionDAO.getQuestionById(i);
     }
     //--------------------Methods----------------------------
 
