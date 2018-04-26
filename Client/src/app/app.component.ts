d22bd144d82ba346f79fe54f686c31e782e0d001
rubenwilhelmsen <rubenwilhelmsen@live.se>
2018-04-26 15:34:24 +0200

    la till log out

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -37,6 +37,10 @@ export class MyApp {
     });
   }
 
+  logOut() {
+    this.nav.setRoot(LoginPage);
+  }
+
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
