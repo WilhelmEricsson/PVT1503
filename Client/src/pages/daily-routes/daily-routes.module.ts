b0ad35d428fe35a95b20a0f66c9564e295a8a04b
lutzajs <blakaj.arian@gmail.com>
2018-05-04 14:32:55 +0200

    Gjorde AchievmentPage och Daily Routes Page

---
+++ Client/src/pages/daily-routes/daily-routes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DailyRoutesPage } from './daily-routes';
+
+@NgModule({
+  declarations: [
+    DailyRoutesPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DailyRoutesPage),
+  ],
+})
+export class DailyRoutesPageModule {}
