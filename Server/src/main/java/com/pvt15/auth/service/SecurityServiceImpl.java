8ec9af68948f1a21d893105c45ae32208aec7b4e
HampusLarsson <larssonhampus@live.se>
2018-05-07 16:09:14 +0200

    Uppdaterat inloggning

--- Server/src/main/java/com/pvt15/auth/service/SecurityServiceImpl.java
+++ Server/src/main/java/com/pvt15/auth/service/SecurityServiceImpl.java
@@ -3,6 +3,7 @@ package com.pvt15.auth.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityServiceImpl implements SecurityService{
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
