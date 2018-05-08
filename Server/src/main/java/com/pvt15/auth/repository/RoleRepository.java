852ff0385cc2af1ff283bbca11233fe43a4a2f49
HampusLarsson <larssonhampus@live.se>
2018-05-08 13:08:41 +0200

    Tagit bort gammal login

--- Server/src/main/java/com/pvt15/auth/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pvt15.auth.repository;
-
-import com.pvt15.auth.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-
-}
--- Server/src/main/java/com/pvt15/auth/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pvt15.auth.repository;
-
-import com.pvt15.auth.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
