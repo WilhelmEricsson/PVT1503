6178f8af1009544ff05c0eab6a066a2b265939a5
Wilhelm Ericsson <wilhelm__ericsson@hotmail.com>
2018-05-09 16:08:48 +0200

    getAllLightPostsLocations() tillagd i LightPostService samt LightPostController

--- Server/src/main/java/com/pvt15/DB/controller/LightPostController.java
+++ Server/src/main/java/com/pvt15/DB/controller/LightPostController.java
@@ -1,4 +1,31 @@
 package com.pvt15.DB.controller;
 
+
+import com.pvt15.DB.entity.LightPostLocations;
+import com.pvt15.DB.service.LightPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public/resources/lightposts/")
 public class LightPostController {
+    @Autowired
+    private LightPostService lightPostService;
+
+    //--------------------Constructor-------------------------
+    public LightPostController() {
+
+    }
+    //--------------------Methods----------------------------
+
+    @RequestMapping(path = "/locations", method = RequestMethod.GET)
+    public Iterable<LightPostLocations> lightPostLocations(){
+        return lightPostService.getAllLightPostsLocations();
+    }
+
+
+
+
 }
