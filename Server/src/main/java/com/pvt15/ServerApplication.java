b3a5d19e4dc371305690f19a0251415a505aa745
Wilhelm Ericsson <ericssonwilhelm@gmail.com>
2018-05-14 23:27:43 +0200

    Lagt till en metod för att lägga till lyktstolpar, denna är dock mer eller mindre tillfällig då den lägger till samma lyktstolpe om och om igen - detsamma gäller för metoden som lägger till information

--- Server/src/main/java/com/pvt15/ServerApplication.java
+++ Server/src/main/java/com/pvt15/ServerApplication.java
@@ -2,7 +2,10 @@ package com.pvt15;
 
 
 import com.pvt15.DB.entity.Information;
+import com.pvt15.DB.entity.LightPost;
+import com.pvt15.DB.entity.LightPostLocations;
 import com.pvt15.DB.repository.InformationRepository;
+import com.pvt15.DB.repository.LightPostRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -33,12 +36,21 @@ public class ServerApplication {
 
 
 	@Bean
-	CommandLineRunner initData(InformationRepository informationRepository) {
+	CommandLineRunner initData(InformationRepository informationRepository, LightPostRepository lightPostRepository) {
 		return args -> {
-			informationRepository.save(new Information("Gamla stan", " Detta handlar egentligen om gamla stan! Learn how to efficiently build and implement microservices in Spring,\n" +
-					"and how to use Docker and Mesos to push the boundaries. Examine a number of real-world use cases and hands-on code examples.\n" +
-					"Distribute your microservices in a completely new way"));
-			informationRepository.save(new Information("Kungliga slottet", "Detta handlar egentligen om slottet! A no-nonsense guide containing case studies and best practise for Spring Boot"));
+			createInformation(informationRepository);
+			createLightPost(lightPostRepository);
 		};
+	}
+	private void createInformation(InformationRepository informationRepository){
+		informationRepository.save(new Information("Gamla stan", " Detta handlar egentligen om gamla stan! Learn how to efficiently build and implement microservices in Spring,\n" +
+				"and how to use Docker and Mesos to push the boundaries. Examine a number of real-world use cases and hands-on code examples.\n" +
+				"Distribute your microservices in a completely new way"));
+		informationRepository.save(new Information("Kungliga slottet", "Detta handlar egentligen om slottet! A no-nonsense guide containing case studies and best practise for Spring Boot"));
+	}
+
+	private void createLightPost(LightPostRepository lightPostRepository){
+		LightPost lightPost = new LightPost(0,"FFFF", new LightPostLocations("59.3293", "18.0686"));
+		lightPostRepository.save(lightPost);
 	}
 }
