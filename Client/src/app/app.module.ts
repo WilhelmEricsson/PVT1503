6fd7de4253ac8477736cbd561fceef5eb08713e7
HampusLarsson <larssonhampus@live.se>
2018-05-15 13:23:59 +0200

    plockat bort /secret call och delvis fått datahämtning från DB att fungera

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -53,6 +53,7 @@ import {HttpClientModule} from "@angular/common/http";
 
 //JWTOptions
 import {JWT_OPTIONS, JwtModule} from '@auth0/angular-jwt';
+import { InformationProvider } from '../providers/information/information';
 
 
 //Whitelisted URL's for authentication header
@@ -127,7 +128,8 @@ export function jwtOptionsFactory(storage:Storage){
     Geolocation,
     AuthProvider,
     SocialSharing,
-    LightPostProvider
+    LightPostProvider,
+    InformationProvider
   ]
 })
 export class AppModule {}
