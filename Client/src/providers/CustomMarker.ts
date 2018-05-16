dfadfa4ad5098fcf7d5b463a63ad1a455d342415
Wilhelm Ericsson <ericssonwilhelm@gmail.com>
2018-05-16 03:04:44 +0200

    har lagt till så att klassen innehåller en id-variabel, därmed ändrat i konstruktorn

--- Client/src/providers/CustomMarker.ts
+++ Client/src/providers/CustomMarker.ts
@@ -1,9 +1,11 @@
 export class CustomMarker {
+    id:number
     lat: number;
     lng: number;
     visited: boolean;
 
-    constructor(lat: number, lng: number) {
+    constructor(id:number, lat: number, lng: number) {
+        this.id = id;
         this.lat = lat;
         this.lng = lng;
         this.visited = false;
