23e60e7fe53ee982dc01576236bf07c7cf46ed7c
WilhelmEricsson <wilhelm__ericsson@hotmail.com>
2018-05-22 12:17:16 +0200

    Update InformationController.java

--- Server/src/main/java/com/pvt15/DB/controller/InformationController.java
+++ Server/src/main/java/com/pvt15/DB/controller/InformationController.java
@@ -37,11 +37,11 @@ public class InformationController {
         // This returns a JSON or XML with the users
         return informationRepository.findAll();
     }
-    @GetMapping("/lightposts/{id}")
+    @GetMapping("/{id}")
     public @ResponseBody Iterable<Information> getInformationByLightPostId(@PathVariable("id") Long lightPostId) {
         return informationRepository.findByLightPostId(lightPostId);
     }
-    @GetMapping("/lightposts/test")
+    @GetMapping("/test")
     public @ResponseBody String getInformationByLightPostId() {
         return "TEST,TEST,TEST";
     }
