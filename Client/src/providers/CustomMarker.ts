968491c25ed6c79a7ad4aaec22bf6aaa4a6bfe99
Wilhelm Ericsson <wilhelm__ericsson@hotmail.com>
2018-05-24 16:29:51 +0200

    lagt till en variabel numofuserspresent

--- Client/src/providers/CustomMarker.ts
+++ Client/src/providers/CustomMarker.ts
@@ -2,10 +2,12 @@ export class CustomMarker {
     id:number
     lat: number;
     lng: number;
+    numOfUsersPresent: number;
 
-    constructor(id:number, lat: number, lng: number) {
+    constructor(id:number, lat: number, lng: number, numOfUsersPresent:number) {
         this.id = id;
         this.lat = lat;
         this.lng = lng;
+        this.numOfUsersPresent = numOfUsersPresent;
     }
 }
