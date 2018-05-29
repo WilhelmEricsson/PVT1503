bcd475acc806a307279fb5396c177ed982fd9006
Wilhelm Ericsson <wilhelm__ericsson@hotmail.com>
2018-05-30 01:54:51 +0200

    lagt till endpoint för att uppdatera,hämta information om, och ta bort användare

--- Server/src/main/java/com/pvt15/security/controller/UserController.java
+++ Server/src/main/java/com/pvt15/security/controller/UserController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/User")
+@RequestMapping("/user")
+@CrossOrigin
 public class UserController{
+
     @Autowired
     UserService userService;
 
@@ -16,16 +18,26 @@ public class UserController{
 
     }
 
-    @GetMapping("/{userName}")
+    @GetMapping("/{userName}/details")
     public User getUserInformation(@PathVariable String userName){
          return userService.getUserInformation(userName);
 
     }
 
-    @PostMapping("/{userName")
+    @PostMapping("/{userName}/delete")
     public boolean deleteUser(@PathVariable String userName){
         return userService.deleteUser(userName);
     }
+
+    @PostMapping("/{userName}/update/email")
+    public User updateUserInformationEmail(@RequestBody String userName, @RequestBody String newEmail){
+        return userService.updateUserEmail(userName, newEmail);
+    }
+    @PostMapping("/{userName}/update/password")
+    public User updateUserInformationPass(@RequestBody String userName, @RequestBody String newPassword){
+        return userService.updateUserEmail(userName, newPassword);
+    }
+
 
 
 }
